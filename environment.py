6107e77c035bd5c938cca92e10791dfff311799d
alex <alex.giorev@gmail.com>
2019-06-07 17:20:07 +0300

    implemented environment.py

--- environment.py
+++ environment.py
@@ -1,15 +1,46 @@
 class Environment:
-    """A namespace is a mapping from variables (symbols) to scheme objects. An environment
-    is a sequence of namespaces."""
-
     def __init__(self, variables, values, parent):
-        raise NotImplementedError
+        """
+        @variables must be an iterable of variables
+        @values must be an iterable. (most commonly scheme objects)
+        @parent_environment must be an Environment or None
+        """
+        
+        self.namespace = dict(zip(variables, values))
+        self.parent = parent
     
     def lookup(self, var):
-        raise NotImplementedError
+        """returns the value corresponding to @var in @self if @var is not bound in @self,
+        raises a LookupError"""
+        
+        namespace = self.first_namespace_that_binds_the_var(var)
+        if namespace is None:
+            raise LookupError(f'the variable "{var}" is not bound in this environment')
+        return namespace[var]
 
     def set_variable_value(self, var, value):
-        raise NotImplementedError
+        """if @var is bound in @self, changes it's corresponding value to @value
+        otherwise, a LookupError is raised"""
+        
+        namespace = self.first_namespace_that_binds_the_var(var)
+        if namespace is None:
+            raise LookupError(f'cannot set the variable "{var}" to the value {value}: '
+                              'the variable is not bound in the current environment')
+        namespace[var] = value
 
     def define_variable(self, var, value):
-        raise NotImplementedError
+        """if @var is bound in @self's namespace, rebinds it to @value
+        otherwise, creates a new binding var -> value"""
+        self.namespace[var] = value
+        
+    def __iter__(self):
+        current_env = self
+        while current_env:
+            yield current_env
+            current_env = current_env.parent
+    
+    def first_namespace_that_binds_the_var(self, var):
+        for current_env in self:
+            if var in current_env.namespace:
+                return current_env.namespace
+        return None
