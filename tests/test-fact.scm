96a0d3eb700a0d7ff01d00cfdb71e0369c3278eb
alex <alex.giorev@gmail.com>
2019-06-12 23:01:12 +0300

    fixed mistakes

--- tests/test-fact.scm
+++ tests/test-fact.scm
@@ -1,11 +1,11 @@
 (define (fact-rec n)
   (if (= n 0)
       1
-      (* n (fact (sub1 n)))))
+      (* n (fact-rec (sub1 n)))))
 
 (define (fact-iter n)
   (define (iter acc k)
     (if (= k 0)
         acc
         (iter (* acc k) (- k 1))))
-  (iter n))
+  (iter 1 n))
